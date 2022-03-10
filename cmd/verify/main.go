04695cba1c4015607c6c7e09c3bfdaa20ef3e86c
Bo Yao <by677@nyu.edu>
2022-03-10 22:37:36 +0800

    fix witness problem

--- cmd/verify/main.go
+++ cmd/verify/main.go
@@ -84,10 +84,7 @@ func myVerify2(p bn254groth16.Proof, vk bn254groth16.VerifyingKey, w bn254witnes
 	fmt.Println(vk_x)
 	for i := 0; i < input.Len(); i++ {
 		var k big.Int
-		fmt.Println(new(bn254.G1Affine).ScalarMultiplication(&mvk.IC[i+1], w[i].ToBigInt(&k)))
-		fmt.Println(new(bn254.G1Affine).ScalarMultiplication(&mvk.IC[i+1], big.NewInt(35)))
-		// TODO: should be w[i].ToBigInt(&k), but then result was incorrect. if use 35 (original input) to replace the witness, result is correct
-		vk_x.Add(&vk_x, new(bn254.G1Affine).ScalarMultiplication(&mvk.IC[i+1], big.NewInt(35)))
+		vk_x.Add(&vk_x, new(bn254.G1Affine).ScalarMultiplication(&mvk.IC[i+1], w[i].ToBigIntRegular(&k)))
 	}
 
 	vk_x.Add(&vk_x, &mvk.IC[0])
@@ -128,6 +125,33 @@ func pairing(
 }
 
 func main() {
+	var alpha bn254.G1Affine
+	ax, _ := new(big.Int).SetString("11277869759378526546969124575771074807958698845834795760110860050010070883649", 10)
+	alpha.X.SetBigInt(ax)
+	ay, _ := new(big.Int).SetString("21096352847591364686617688592277265927409501086196542200785515846892919186269", 10)
+	alpha.Y.SetBigInt(ay)
+	fmt.Println("---")
+	fmt.Println(&alpha)
+	var aj bn254.G1Jac
+	aj.FromAffine(&alpha)
+	fmt.Println(&aj)
+	// xm := alpha.X.ToRegular()
+	// fmt.Println(&xm)
+	var beta bn254.G2Affine
+	bx0, _ := new(big.Int).SetString("11758142261403163295608845506555627774828420371736554132879876329100319737215", 10)
+	bx1, _ := new(big.Int).SetString("7554225344334507119311023967766263980488693208432826234736374432532790891870", 10)
+	by0, _ := new(big.Int).SetString("16838074971068588345631214622994804945725658374885832516183540283152629504137", 10)
+	by1, _ := new(big.Int).SetString("13629812609392235503574289614193324689551363161071724741437706155184644607605", 10)
+
+	beta.X.A0.SetBigInt(bx0)
+	beta.X.A1.SetBigInt(bx1)
+	beta.Y.A0.SetBigInt(by0)
+	beta.Y.A1.SetBigInt(by1)
+	vke, err := bn254.Pair([]bn254.G1Affine{alpha}, []bn254.G2Affine{beta})
+	fmt.Println(&beta)
+
+	fmt.Println(&vke)
+
 	f, _ := os.Open("proof")
 	defer f.Close()
 	var p bn254groth16.Proof
@@ -137,13 +161,16 @@ func main() {
 	defer f2.Close()
 	var vk bn254groth16.VerifyingKey
 	vk.ReadFrom(f2)
+	fmt.Println("vkalpha")
+	fmt.Println(&vk.G1.Alpha.X)
+	fmt.Println(vk.G1.Alpha.X.ToRegular())
 
 	f3, _ := os.Open("witness")
 	defer f3.Close()
 	var w bn254witness.Witness
 	w.ReadFrom(f3)
 
-	err := bn254groth16.Verify(&p, &vk, w)
+	err = bn254groth16.Verify(&p, &vk, w)
 	if err != nil {
 		panic("verify fail")
 	} else {
